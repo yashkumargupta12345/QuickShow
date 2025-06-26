4d90d3231d1fb6b7e80d34d98b50376d22f9eaf5
yashkumargupta12345 <guptayashyg45227@gmail.com>
2025-06-27 00:33:31 +0530

    Completed the Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
   )
 }
 
